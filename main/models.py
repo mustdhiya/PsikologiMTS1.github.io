b71e9bc9798c27c764ccd2ee2fa052b1b5d2badd
Must.dhiya <fauzan.ilham2112@gmail.com>
2025-08-21 16:31:38 +0800

    update

--- main/models.py
+++
@@ -1,9 +0,0 @@
-# main/models.py
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-from django.utils import timezone
-from django.core.validators import MinValueValidator, MaxValueValidator
-import uuid
-import hashlib
-import secrets
-
--- psikologimts1/urls.py
+++ psikologimts1/urls.py
@@ -2,7 +2,7 @@
 URL configuration for psikologimts1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls.static import static
+
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # Routing utama ke app Anda
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
