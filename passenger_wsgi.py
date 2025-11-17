7376418f462ee0aba8a496be68a46d37b7beff30
Must.dhiya <fauzan.ilham2112@gmail.com>
2025-11-18 00:36:08 +0800

    up deh

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,18 @@
-import imp
-import os
-import sys
-sys.path.insert(0, "/home/prep8924/python/PsikologiMTS1.github.io/psikologimts1")
+import sys, os
 
-import os
+# PyMySQL
+import pymysql
+pymysql.install_as_MySQLdb()
+
+# Virtual environment
+sys.path.insert(0, '/home/prep8924/virtualenv/python/PsikologiMTS1.github.io/3.11/lib/python3.11/site-packages')
+
+# Django project
+sys.path.insert(0, '/home/prep8924/python/PsikologiMTS1.github.io')
+
+# Django settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'psikologimts1.settings'
 
+# WSGI
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
